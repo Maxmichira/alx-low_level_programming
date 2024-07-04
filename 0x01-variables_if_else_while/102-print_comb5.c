6b0d7c9ba89016d8a881d2a63e49b4e3b12b3f70
Maxmichira <maxmichira@outlook.com>
2024-07-04 05:35:17 +0300

    Fix Betty style issues in 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible combinations of two
+ * two-digit numbers, ranging from 00 to 99. The numbers are printed
+ * in ascending order and separated by a comma followed by a space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
